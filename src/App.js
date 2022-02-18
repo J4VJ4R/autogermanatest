a8fe957d8ecae9d0b1f0a994a8acd7d1c64090ec
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2022-02-18 15:21:19 -0500

    actualización de la barra de busqueda

--- src/App.js
+++ src/App.js
@@ -2,27 +2,75 @@ import React, {useEffect, useState} from 'react'
 import './App.css';
 import axios from "axios"
 import "bootstrap/dist/css/bootstrap.min.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faSearch} from '@fortawesome/free-solid-svg-icons';
 function App() {
-  const url = 'https://jsonplaceholder.typicode.com/todos/'
-  const [groups, setGroup] = useState()
-  const fetchApi = async () => {
-    const response = await fetch(url)
-    const responseJSON = await response.json()
-    setGroup(responseJSON)
+  
+  const [users, setUsers]= useState([])
+  const [tableUsers, setTableUsers]= useState([])
+  const [search, setSearch]= useState("")
+
+  const requestGet=async()=>{
+    await axios.get("https://jsonplaceholder.typicode.com/todos/")
+    .then(response=>{
+      setUsers(response.data);
+      setTableUsers(response.data);
+    }).catch(error=>{
+      console.log(error);
+    })
+  }
+
+  const handleChange=e=>{
+    setSearch(e.target.value)
+    filter(e.target.value)
+  }
+  const filter=(itemSearch)=>{
+    let resultsSearch=tableUsers.filter((item)=>{
+      if(item.userId.toString().toLowerCase().includes(itemSearch.toLowerCase())
+        || item.id.toString().toLowerCase().includes(itemSearch.toLowerCase())
+        || item.title.toString().toLowerCase().includes(itemSearch.toLowerCase())){
+        return item
+      }
+    })
+    setUsers(resultsSearch)
   }
   useEffect(() => {
-    fetchApi()
-  }, [])
+    requestGet();
+  },[])
   return (
     <div className="App">
-      hola mundo 2
-      <ul>
-        { !groups ? 'Cargandoo...' : 
-        groups.map((group, index)=>{
-          return <li key={index}>{group.title} {group.completed ? ' ✅': '❌'}</li>
-        })
-        }
-      </ul>
+    <div className="containerInput container pt-3">
+      <input type="text" className="form-control inputSearch" value={search} placeholder="Búsqueda por User Id, Id y Tarea"
+      onChange={handleChange} />
+      <button className="btn btn-success">
+        <FontAwesomeIcon icon={faSearch}/>
+      </button>
+    </div>
+      <div className="table-responsive">
+        <table className="table table-sm table-bordered">
+          <thead>
+            <tr>
+              <th>User Id</th>
+              <th>id</th>
+              <th>Tarea</th>
+              <th>Estado</th>
+            </tr>
+          </thead>
+          <tbody>
+            { users && 
+              users.map((user)=>(
+                /* return <li key={index}>{group.title} {group.completed ? ' ✅': '❌'}</li> */
+                <tr key={user.id}>
+                  <td>{user.userId}</td>
+                  <td>{user.id}</td>
+                  <td>{user.title}</td>
+                  <td>{user.completed ? ' ✅': '❌'}</td>
+                </tr>
+              ))}
+          </tbody>
+          
+        </table>
+      </div>
     </div>
   );
 }
