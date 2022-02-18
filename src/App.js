e155465c5701ad1150dc2cf62f3bef39c0c7b291
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2022-02-18 07:23:09 -0500

    added api and show data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react'
 import './App.css';
-
+import axios from "axios"
+import "bootstrap/dist/css/bootstrap.min.css"
 function App() {
+  const url = 'https://jsonplaceholder.typicode.com/todos/'
+  const [groups, setGroup] = useState()
+  const fetchApi = async () => {
+    const response = await fetch(url)
+    const responseJSON = await response.json()
+    setGroup(responseJSON)
+  }
+  useEffect(() => {
+    fetchApi()
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hola mundo 2
+      <ul>
+        { !groups ? 'Cargandoo...' : 
+        groups.map((group, index)=>{
+          return <li key={index}>{group.title} {group.completed ? ' ✅': '❌'}</li>
+        })
+        }
+      </ul>
     </div>
   );
 }
-
 export default App;
